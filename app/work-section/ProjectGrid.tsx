cfab57fd7dd0eba9f89c5a8d89d73216a227705f
Yoan Gilliand <yoangilliand@gmail.com>
2025-02-16 01:31:26 +0100

    Translate projects

--- app/work-section/ProjectGrid.tsx
+++ app/work-section/ProjectGrid.tsx
@@ -1,27 +1,29 @@
 import ProjectCard from "./ProjectCard";
 import { devProjects, ProjectProps } from "./projectDetails";
+import { useTranslation } from "react-i18next";
 
 const ProjectGrid = () => {
+    const { t } = useTranslation();
 
-  return (
-    <>
-        <div className="grid w-[90%] grid-cols-1 grid-rows-2 gap-y-10 gap-x-6 lg:max-w-[1200px] lg:grid-cols-1">
-          {devProjects.map((project: ProjectProps) => (
-            <ProjectCard
-              id={project.id}
-              key={project.id}
-              name={project.name}
-              description={project.description}
-              technologies={project.technologies}
-              github={project.github}
-              demo={project.demo}
-              image={project.image}
-              available={project.available}
-            />
-          ))}
-        </div>
-    </>
-  );
+    return (
+        <>
+            <div className="grid w-[90%] grid-cols-1 grid-rows-2 gap-y-10 gap-x-6 lg:max-w-[1200px] lg:grid-cols-1">
+                {devProjects.map((project: ProjectProps) => (
+                    <ProjectCard
+                        id={project.id}
+                        key={project.id}
+                        name={t(project.nameKey)} // Fetch translated name
+                        description={t(project.descriptionKey)} // Fetch translated description
+                        technologies={project.technologies}
+                        github={project.github}
+                        demo={project.demo}
+                        image={project.image}
+                        available={project.available}
+                    />
+                ))}
+            </div>
+        </>
+    );
 };
 
 export default ProjectGrid;
--- app/work-section/projectDetails.ts
+++ app/work-section/projectDetails.ts
@@ -1,7 +1,7 @@
 export type ProjectProps = {
   id: number;
-  name: string;
-  description: string;
+  nameKey: string;
+  descriptionKey: string;
   technologies: string[];
   github: string;
   demo: string;
@@ -9,11 +9,11 @@ export type ProjectProps = {
   available: boolean;
 };
 
-export const devProjects = [
+export const devProjects: ProjectProps[] = [
   {
     id: 0,
-    name: "MATCH RACE",
-    description: "Match Race is a desktop application designed to streamline photo competition organization for Photo Club Fribourg, featuring automated pairing and voting, real-time audience engagement, and a user-friendly interface.",
+    nameKey: "projects.matchRace.name",
+    descriptionKey: "projects.matchRace.description",
     technologies: ["Electron.js", "Vue.js", "Vuetify"],
     github: "https://gitlab.forge.hefr.ch/ca-tic/match-race",
     demo: "https://www.linkedin.com/in/yoan-gilliand/#:~:text=Projets-,Match%20Race%20%E2%80%93%20A%20Digitalized%20Photo%20Competition%20Platform,-Match%20Race%20%E2%80%93%20A",
@@ -22,8 +22,8 @@ export const devProjects = [
   },
   {
     id: 1,
-    name: "EPARKING",
-    description: "eParking is a web platform designed to modernize the management of parking permit requests for HES-SO//FR, featuring interactive forms, secure authentication, and an admin interface for efficient request handling and data management.",
+    nameKey: "projects.eParking.name",
+    descriptionKey: "projects.eParking.description",
     technologies: ["Vue.js", "Express.js", "MySQL"],
     github: "https://gitlab.forge.hefr.ch/yoan.gilliand/eparking/-/tree/develop",
     demo: "https://www.linkedin.com/in/yoan-gilliand/#:~:text=Autres%20contributeurs-,eParking%20%E2%80%93%20A%20Smart%20Parking%20Permit%20Management%20Platform,-eParking%20%E2%80%93%20A%20Smart",
